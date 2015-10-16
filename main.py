5f98befd462f536fb714b143159dd1a43c3301fa
Adam <adambossy@gmail.com>
2015-10-16 16:25:16 -0700

    Update list() to read from db

--- main.py
+++ main.py
@@ -1,9 +1,29 @@
-from flask import Flask
+import sqlite3
+from flask import Flask, g, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+### Database cruft ripped from the Flask tutorial: http://flask.pocoo.org/docs/0.10/tutorial/
+
+def connect_db():
+    return sqlite3.connect("mira.db")
+
+@app.before_request
+def before_request():
+    g.db = connect_db()
+    g.db.row_factory = sqlite3.Row
+
+@app.teardown_request
+def teardown_request(exception):
+    db = getattr(g, 'db', None)
+    if db is not None:
+        db.close()
+
+
+@app.route('/topic/<int:topic_id>')
+def list(topic_id):
+	links = g.db.execute("SELECT * FROM links ORDER BY id ASC")
+	return render_template('list.html', entries=links)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
